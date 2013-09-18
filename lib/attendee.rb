b8b1e7f2e0df99682f436ebd73b29e8f0894a5cc
Simon Taranto <simon.taranto@gmail.com>
2013-09-18 10:56:07 -0600

    added extra fields to Attendee

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -2,13 +2,17 @@ require './lib/phone_number'
 require './lib/zip_code'
 
 class Attendee
-  attr_accessor :first_name, :last_name, :phone_number, :zip_code
+  attr_accessor :first_name, :last_name, :phone_number, :zip_code, :email, :city, :state, :address
 
   def initialize(input = {})
     @first_name = input[:first_name]
     @last_name = input[:last_name]
     @phone_number = PhoneNumber.new(input[:phone_number]).number
     @zip_code = ZipCode.new(input[:zip_code]).zip_code
+    @email = input[:email]
+    @city = input[:city]
+    @state = input[:state]
+    @address = input[:address]
   end
 
 end
