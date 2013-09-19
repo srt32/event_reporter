39f496e87ad6b12fa328a5b2007505e100aef0c8
Simon Taranto <simon.taranto@gmail.com>
2013-09-19 13:26:24 -0600

    move first_name and las_name cleaning into methods

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -5,14 +5,26 @@ class Attendee
   attr_accessor :first_name, :last_name, :phone_number, :zip_code, :email, :city, :state, :address
 
   def initialize(input = {})
-    @first_name = (input[:first_name]).downcase unless (input[:first_name]).nil?
-    @last_name = (input[:last_name]).downcase unless (input[:first_name]).nil?
+    @first_name = clean_first_name(input[:first_name])
+    @last_name = clean_last_name(input[:last_name])
     @phone_number = PhoneNumber.new(input[:phone_number]).number
     @zip_code = ZipCode.new(input[:zip_code]).zip_code
     @email = (input[:email]).downcase unless (input[:email]).nil?
     @city = (input[:city]).downcase unless (input[:city]).nil?
     @state = (input[:state]).downcase unless (input[:state]).nil?
     @address = (input[:address]).downcase unless (input[:address]).nil?
+  end
+
+  def convert_to_string_and_downcase(input)
+    input.to_s.downcase
+  end
+
+  def clean_first_name(input)
+    convert_to_string_and_downcase(input)
+  end
+
+  def clean_last_name(input)
+    convert_to_string_and_downcase(input)
   end
 
 end
